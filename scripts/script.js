4347023ffaedb6507a6340d66364bff088880802
Pat Johnston <pattrashgarbage@gmail.com>
2018-02-13 12:25:20 -0500

    Begin solving anagrams

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,29 @@ const letterInput = document.querySelector('.letterInput');
 const loadingSpinner = document.querySelector('.spinner01');
 letterSubmissionForm.addEventListener('submit', handleSubmit);
 
+const wordList = ["pat", "patty", "patrick"]
+
 function handleSubmit(event) {
     event.preventDefault();
-    loadAnimation();
+    toggleLoadAnimation();
     findWords(letterInput.value);
 }
 function findWords(letters) {
-    
+    // Create array of letter input for searching
+    const userLetters = letters.split('');
+    console.log(userLetters);
+
+    // FIND A WAY TO NARROW THE LIST BEFOREHAND, WOW
+        // Eliminate all words that start with letters that aren't in userLetters
+        // Eliminate all words that are too long
+        // Elimniate 
+
+    // Run through the remaining array to find appropriate words
+    wordList.map(word => {
+        
+    })
 }
-function loadAnimation() {
-    letterSubmissionForm.classList.add("hideElement");
-    loadingSpinner.classList.remove("hideElement");
+function toggleLoadAnimation() {
+    letterSubmissionForm.classList.toggle("hideElement");
+    loadingSpinner.classList.toggle("hideElement");
 }
