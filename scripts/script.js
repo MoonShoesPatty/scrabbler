f1ab7a2cf3fcedcb13703b3642f040090d9b5e1d
Pat Johnston <pattrashgarbage@gmail.com>
2018-02-13 11:27:49 -0500

    Add loading animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
 const letterSubmissionForm = document.querySelector('.letterSubmission');
 const letterInput = document.querySelector('.letterInput');
+const loadingSpinner = document.querySelector('.spinner01');
 letterSubmissionForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
+    loadAnimation();
     findWords(letterInput.value);
 }
 function findWords(letters) {
     
+}
+function loadAnimation() {
+    letterSubmissionForm.classList.add("hideElement");
+    loadingSpinner.classList.remove("hideElement");
 }
